c060b3f9937f21fe89121175469ddd2fb31939b9
CarloBattista <carlitobatti@gmail.com>
2023-04-14 16:30:30 +0200

    array di oggetti con le bici, proprietà nome e peso

--- js_snack_1/_assets/js/main.js
+++ js_snack_1/_assets/js/main.js
@@ -7,3 +7,28 @@ Stampare a schermo la bici con peso minore utilizzando destructuring e template 
 
 */
 
+// Array oggetti
+const biciDaCorsa = [
+    {
+        nome: 'Bici gialla',
+        peso: '7',
+    }, {
+        nome: 'Bici rossa',
+        peso: '12',
+    }, {
+        nome: 'Bici blu',
+        peso: '11',
+    }, {
+        nome: 'Bici arancione',
+        peso: '8',
+    }, {
+        nome: 'Bici marrone',
+        peso: '4',
+    }, {
+        nome: 'Bici nera',
+        peso: '19',
+    }, {
+        nome: 'Bici bianca',
+        peso: '17',
+    }
+];
