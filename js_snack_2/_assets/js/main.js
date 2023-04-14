7324272bc93085d3851b2875bcc5c4f5162400aa
CarloBattista <carlitobatti@gmail.com>
2023-04-14 17:09:52 +0200

    creato array squadre

--- js_snack_2/_assets/js/main.js
+++ js_snack_2/_assets/js/main.js
@@ -10,3 +10,40 @@ Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 */
+
+// Array oggetti
+const squadre = [
+    {
+        nome: 'Juventus',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    }, {
+        nome: 'Milan',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    }, {
+        nome: 'Napoli',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    }, {
+        nome: 'Inter',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    }, {
+        nome: 'Roma',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    }, {
+        nome: 'Lazio',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    }, {
+        nome: 'Atalanta',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    }, {
+        nome: 'Fiorentina',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    },
+];
